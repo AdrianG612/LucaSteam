c278473fc238cb24e38582a3c37926950001125b
PatriciaGS00 <patri.adri.98@gmail.com>
2024-11-20 13:15:49 +0100

    juegos

--- LucaSteamJunto/src/datos/JuegosImp.java
+++ LucaSteamJunto/src/datos/JuegosImp.java
@@ -43,26 +43,36 @@ public class JuegosImp implements Juegos{
 		this.editores = editores;
 	}
 
-	@Override
-	public boolean editorExiste(Editor e) {
-		if (e == null || editorExiste(e.getNombre())) {
-			logger.warn("El editor es nulo.");
-			return false;
-		}else
-			return editores.contains(e);
-	}
-
+	//metodos gestion de EDITOR
+	
 	@Override
 	public boolean editorExiste(String nombre) {
+		//comprobar string valido
 		if (nombre == null || nombre.isEmpty()) {
 	        logger.warn("El nombre del editor es nulo o vacío.");
 	        return false;
 	    }
 	    for (Editor editor : editores) {
+	    	//comprobar si esta en la lista
 	        if (editor.getNombre().equalsIgnoreCase(nombre))
 	            return true;
 	    }
 	    return false;
+	}
+	
+	@Override
+	public boolean editorExiste(Editor e) {
+		//Comprobar editor no nulo
+		if (e == null ) {
+			logger.warn("El editor es nulo.");
+			return false;
+		}
+		//comprobar por nombre
+		else if (editorExiste(e.getNombre())) {
+			logger.warn("El editor "+e.getNombre()+" ya existe.");
+			return true;
+		}			
+		return editores.contains(e);
 	}
 
 	@Override
@@ -80,12 +90,57 @@ public class JuegosImp implements Juegos{
 	     editores.add(e);
 	     return true;
 	}
+	
+	//metodos gestion JUEGO
+	
+	@Override
+	public boolean juegoExiste(String nombre) {
+		//comprobar string valido
+		if (nombre == null || nombre.isEmpty()) {
+			logger.warn("El nombre del juego es nulo o vacío.");
+			return false;
+		}
+		//comprobar si esta en la lista
+		for (Juego juego : juegos) {
+			if (juego.getNombre().equalsIgnoreCase(nombre))
+				return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public boolean juegoExiste(Juego j) {
+		//comprobar juego no nulo
+		if (j == null ) {
+			logger.warn("El juego es nulo.");
+			return false;
+		}
+		//comprobar por nombre
+		else if (juegoExiste(j.getNombre())) {
+			logger.warn("El editor "+j.getNombre()+" ya existe.");
+			return true;
+		}			
+		return juegos.contains(j);
+	}
 
 	@Override
 	public boolean addJuego(Juego j) {
-		//completarrrrr
-		return false;
+		//comprobar juego y nombre del juego no nulos 
+		if (j == null || j.getNombre() == null || j.getNombre().isEmpty()) {
+            logger.warn("No se puede añadir un juego nulo o con nombre vacío.");
+            return false;
+        }
+		//comprobar que no este ya en la lista
+        if (juegos.contains(j)) {
+            logger.warn("El juego \"" + j.getNombre() + "\" ya existe.");
+            return false;
+        }
+        //agregar j a la lista de juegos
+        juegos.add(j);
+        return true;
 	}
+	
+	//OTROS
 	
 	@Override
 	public String toString() {
@@ -93,15 +148,11 @@ public class JuegosImp implements Juegos{
 	}
 
 	@Override
-	public boolean juegoExiste(String editor) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
 	public List<Juego> filtrarGeneroPlataforma() {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	
 	
 }
